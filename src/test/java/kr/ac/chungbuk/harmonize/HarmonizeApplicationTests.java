879e388f0bd47b74b30187885828c2ddaa8b22a3
Woochang Lee <changi112242@gmail.com>
2024-03-16 18:10:55 +0900

    Initial commit

---
+++ src/test/java/kr/ac/chungbuk/harmonize/HarmonizeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.chungbuk.harmonize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
