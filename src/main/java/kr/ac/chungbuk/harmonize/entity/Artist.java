faab421f9248ac890c234de8e098cc77ac40954a
Woochang Lee <changi112242@gmail.com>
2024-06-18 23:24:38 +0900

    feat: 가수 entity 생성 #2

---
+++ src/main/java/kr/ac/chungbuk/harmonize/entity/Artist.java
@@ -0,0 +1,33 @@
+package kr.ac.chungbuk.harmonize.entity;
+
+import jakarta.persistence.*;
+import kr.ac.chungbuk.harmonize.enums.Gender;
+import lombok.Data;
+
+@Entity
+@Data
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long artistId;
+
+    @Column(length = 50, nullable = false)
+    private String artistName;                  // 가수 이름
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;                      // 가수 성별
+
+    @Column(length = 100)
+    private String activityPeriod;              // 활동 연대
+
+    @Column(length = 100)
+    private String nation;                      // 국적
+
+    @Column(length = 100)
+    private String agency;                      // 소속사
+
+    @Column(nullable = true)
+    private String profileImage;                // 가수 프로필 이미지
+}
---
+++ src/main/java/kr/ac/chungbuk/harmonize/enums/Gender.java
@@ -0,0 +1,28 @@
+package kr.ac.chungbuk.harmonize.enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Gender {
+    MALE,       // 남성
+    FEMALE,     // 여성
+    ETC;        // 그외
+
+
+    private static final Map<String, Gender> values = new HashMap<>();
+    static {
+        for (Gender g : Gender.values()) {
+            if (values.put(g.name(), g) != null) {
+                throw new IllegalArgumentException("duplicate value: " + g.name());
+            }
+        }
+    }
+
+    public static Gender fromString(String value) throws IllegalArgumentException {
+        Gender genre = values.get(value);
+        if (values.get(value) == null)
+            throw new IllegalArgumentException("invalid genre string");
+        else
+            return genre;
+    }
+}
