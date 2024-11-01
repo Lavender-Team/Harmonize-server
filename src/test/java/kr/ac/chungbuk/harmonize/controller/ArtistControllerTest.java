74fbbe1375fa7411f9fdf881b9e1d2730c951a1c
Jae Won Bae <49405805+JaewonB37@users.noreply.github.com>
2024-11-01 11:41:24 +0900

    #8 @WithMockUser 추가

--- src/test/java/kr/ac/chungbuk/harmonize/controller/ArtistControllerTest.java
+++ src/test/java/kr/ac/chungbuk/harmonize/controller/ArtistControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.FileInputStream;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(authorities = "ADMIN")
 class ArtistControllerTest {
 
     @Autowired
